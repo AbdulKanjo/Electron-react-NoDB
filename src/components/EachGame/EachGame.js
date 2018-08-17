978e83a813d2b193fe106eb4dfc5727dbd7ebd61
Abdul Kanjo <Abdul.kanjo@gmail.com>
2018-08-17 13:01:21 -0500

    router working

---
+++ src/components/EachGame/EachGame.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EachGame extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>EachGame</div>;
+  }
+}
+
+export default EachGame;
